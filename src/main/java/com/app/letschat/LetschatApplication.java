bd10a270f6ce6220c327a01033185a74a2304fd1
Yulian Belov <belovyulian@gmail.com>
2023-06-28 13:34:43 +0300

    first commit

---
+++ src/main/java/com/app/letschat/LetschatApplication.java
@@ -0,0 +1,13 @@
+package com.app.letschat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetschatApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LetschatApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/app/letschat/LetschatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.letschat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetschatApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
